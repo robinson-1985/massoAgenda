625a32264ffc76a729358af2fc6a3b0358eeaba8
robinson <robinpaulista85@gmail.com>
2023-08-20 20:12:40 -0300

    Modificando arquivos

--- agenda/models.py
+++ agenda/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
@@ -20,6 +22,8 @@ class Agendamento(models.Model):
     massoterapeuta = models.ForeignKey(Massoterapeuta, on_delete=models.CASCADE)
     data_hora = models.DateTimeField()
     observacoes = models.TextField()
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return f"Agendamento de {self.cliente.nome} com {self.massoterapeuta.nome} em {self.data_hora}"
