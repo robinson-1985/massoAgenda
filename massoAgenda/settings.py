c5d6acfaf97b7684d915cbab5f411efcd0130921
Rafael Cassau <rafael.cassau@loadsmart.com>
2024-06-07 16:34:29 -0300

    Fix settings STATIC_ROOT and STATIC_URL that were defined as tuple due to an extra comma typo, remove all the junk in the requisitos.txt

--- massoAgenda/settings.py
+++ massoAgenda/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -117,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/',
-STATIC_ROOT = BASE_DIR / 'staticfiles',
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
